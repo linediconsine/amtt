ac04f1960476b79b81f44278cb9a2df023714a4b
linediconsine <linediconsine@gmail.com>
2014-05-25 20:13:10 +0200

    Conflicts resolved

--- amtt04.js
+++ amtt04.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 //
 // version 004 pre-beta
 // Code by Marco Amato, released in AGPL (http://www.gnu.org/licenses/agpl-3.0.html)
@@ -8,49 +7,24 @@
 // __src_img == where to find images for the test
 
 
-function test_connections(_this,fallback){
-	// __src_img == where to find images for the test
-=======
-
-
 
 function test_connections(_this,fallback){
 	// where to find images for the test
->>>>>>> 323ff69507b41205c23d1137cc00b67936ab6172
-=======
-
-
-
-function test_connections(_this,fallback){
-	// where to find images for the test
->>>>>>> 323ff69507b41205c23d1137cc00b67936ab6172
 	__src_img = "https://raw.githubusercontent.com/linediconsine/amtt/master/240/amtt/amtt00"; 
 	if(typeof _this != "undefined"){
 		var __d2 = new Date();
 		var __time2 = String(__d2.getTime()); 
 		__time_spent = parseInt(__time2) - parseInt(_this.id);
 		__Bs = __time_spent  * 1000;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-		console.log(__Bs + ' B/s');
-		
-=======
-=======
->>>>>>> 323ff69507b41205c23d1137cc00b67936ab6172
-		
 		console.log(_this.id);
 		console.log(__time2);
-		
+
 		console.log(parseInt(_this.id)); 
 		console.log(parseInt(__time2)); 
-	
+
 		console.log(__time_spent);
 		console.log((__time_spent  * 1000) + ' B/s');
-<<<<<<< HEAD
->>>>>>> 323ff69507b41205c23d1137cc00b67936ab6172
-=======
->>>>>>> 323ff69507b41205c23d1137cc00b67936ab6172
 	if(typeof fallback != "undefined"){
 		fallback(__Bs);
 		}
@@ -59,22 +33,14 @@ function test_connections(_this,fallback){
 		if(__rnd.length < 2){
 			__rnd='0' + String(__rnd);
 		}
-		
+
 		var __d = new Date();
 		var __time = String(__d.getTime()); 
-		
+
 		var elem  = document.createElement('img');
     	elem.setAttribute('src', __src_img +  __rnd + '.jpg');
 		elem.setAttribute('id', __time); // Time when the test start;
     	elem.setAttribute('onload','test_connections(this);');
 	}
 };
-<<<<<<< HEAD
-<<<<<<< HEAD
-test_connections();
-=======
 test_connections()
->>>>>>> 323ff69507b41205c23d1137cc00b67936ab6172
-=======
-test_connections()
->>>>>>> 323ff69507b41205c23d1137cc00b67936ab6172
